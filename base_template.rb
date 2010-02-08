20023f105f91bf2fa159b4f99a4910b792666b13
Mark Sands <mark@mark.local>
2010-02-07 22:53:29 -0600

    added pickle. updated gem versions. added a few files to .gitignore

--- base_template.rb
+++ base_template.rb
@@ -11,11 +11,16 @@ if yes?("Do you want to use cucumber?")
   plugin 'rspec-rails', :git => "git://github.com/dchelimsky/rspec-rails.git"
   plugin 'webrat', :git => "git://github.com/brynary/webrat.git" 
   plugin 'cucumber', :git => "git://github.com/aslakhellesoy/cucumber.git" 
-  
-  gem "rspec", :lib => false, :version => ">=1.2.2"
-  gem "rspec-rails", :lib => false, :version => ">=1.2.2"
-  gem "webrat", :lib => false, :version => ">=0.4.3"
-  gem "cucumber", :lib => false, :version => ">=0.3.0"
+    
+  gem "rspec", :lib => false, :version => ">=1.2.9"
+  gem "rspec-rails", :lib => false, :version => ">=1.2.9"
+  gem "webrat", :lib => false, :version => ">=0.5.3"
+  gem "cucumber", :lib => false, :version => ">=0.4.3"
+
+  if yes?("With pickle?")
+    plugin 'pickle', :git => "git://github.com/ianwhite/pickle.git" 
+    gem "pickle", :lib => false, :version => ">=0.1.21"  
+  end
   
   generate :cucumber
 elsif yes?("Would you rather just use RSpec?")
@@ -36,6 +41,8 @@ file ".gitignore", <<-END
   tmp/**/*
   config/database.yml
   db/*.sqlite3
+  README
+  public/index.html
 END
 
 run "touch tmp/.gitignore log/.gitignore vendor/.gitignore config/authorization_rules.rb"
@@ -45,8 +52,6 @@ git :add => ".", :commit => "-m 'initial commit'"
 
 generate :controller, "home index"
 route "map.root :controller => 'home'"
-git :rm => "public/index.html"
-git :rm => "README"
 
 git :add => ".", :commit => "-m 'adding home controller'"
 
@@ -54,8 +59,10 @@ if yes?("Do you want user integration?")
   generate :nifty_scaffold, 'user username:string email:string password:string new edit'
   generate :session, 'user_session'
   generate :nifty_scaffold, 'user_session --skip-model username:string password:string new destroy'
+
   route "map.login 'login', :controller => 'user_sessions', :action => 'new'"
   route "map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'"
   route "map.signup 'signup', :controller => 'users', :action => 'new'"
+
   git :add => ".", :commit => "-m 'adding authlogic'"
 end
